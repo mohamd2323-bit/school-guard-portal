550288b68e74d901bd76c156155ab764571c97e3
mohamd2323-bit <mohamd2323@googlemail.com>
2026-07-19 13:47:24 +0300

    Update index.ts

--- lib/db/src/index.ts
+++ lib/db/src/index.ts
@@ -1,6 +1,6 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import pg from "pg";
-import * as schema from "./schema/index.js";
+import * as schema from "./schema.js";
 
 const { Pool } = pg;
 
@@ -16,4 +16,4 @@ export const pool = new Pool({
 
 export const db = drizzle(pool, { schema });
 
-export * from "./schema/index.js";
+export * from "./schema.js";
